ea7c99890dcee3180f366c9ff8a52ec9d7e1f907
flobernier <florian.bernier@protonmail.com>
2021-09-03 18:33:01 +0200

    Add comments to helper functions

--- helper.py
+++ helper.py
@@ -4,35 +4,51 @@ import cv2 as cv
 import math
 
 
+# @brief	Get mean RGB values for an image
+# @param[in]	img Source image
+# @return	Array with RGB values between 0 and 255
 def getRGBMean(img):
 	channels = cv.mean(img) # BGR channels
 	return [round(channels[2]), round(channels[1]), round(channels[0])] # RGB
 
-
+# @brief	Get mean brightness value for an image
+# @param[in]	img Source image
+# @return	Mean brightness
 def getBrightness1(img):
-	# Get Brightness from Grayscale + mean
+	# Brightness from Grayscale + mean
 	gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 	b,g,r,_ = cv.mean(gray_img) # BGR channels
 	return b
 
+# @brief	Get mean perceived brightness value for an image
+# @param[in]	img Source image
+# @return	Mean perceived brightness
 def getBrightness2(img):
 	# Perceived brightness with RGB
 	b,g,r,_ = cv.mean(img) # BRG channels
 	return math.sqrt(0.241*r*r + 0.691*g*g + 0.068*b*b)
 
+# @brief	Get mean brightness value for an image
+# @param[in]	img Source image
+# @return	Mean brightness
 def getBrightness3(img):
-	# Get Brightness from HSV
+	# Brightness from HSV
 	hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 	h,s,v,_ = cv.mean(hsv_img)
 	return v
 
+# @brief	Get mean saturation value for an image
+# @param[in]	img Source image
+# @return	Mean saturation
 def getSaturation(img):
-	# Get Saturation from HSV
+	# Saturation from HSV
 	hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 	h,s,v,_ = cv.mean(hsv_img)
 	return s
 
-
+# @brief	Get blur level for an image
+# @param[in]	img Source image
+# @return	Blur level
 def getBlurLevel(img):
 	# Blur level is the variance of the Laplacian of the image
 	# A higher value mean more focus and less blur
